db03725a87cef67f8ae9a028f4f37b84f5b1db38
Michael Fairley <michaelfairley@gmail.com>
2013-09-25 15:13:33 -0500

    Make the smoke test less flakey

--- smoke.sh
+++ smoke.sh
@@ -1,6 +1,8 @@
 EXPECTED="{'a': 2, 'on': 1, 'great': 1, 'Humpty': 3, 'again': 1, 'wall': 1, 'Dumpty': 2, 'men': 1, 'had': 1, 'all': 1, 'together': 1, \"King's\": 2, 'horses': 1, 'All': 1, \"Couldn't\": 1, 'fall': 1, 'and': 1, 'the': 2, 'put': 1, 'sat': 1}"
 
 python example.py > output &
+sleep 1
 python mincemeat.py -p changeme localhost
+sleep 1
 
 diff <(cat output) <(echo $EXPECTED)
