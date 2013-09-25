85553afedff526ad9ee261f298c59c40d36d001b
Michael Fairley <michaelfairley@gmail.com>
2013-09-25 15:03:28 -0500

    Add simple smoke test

---
+++ smoke.sh
@@ -0,0 +1,6 @@
+EXPECTED="{'a': 2, 'on': 1, 'great': 1, 'Humpty': 3, 'again': 1, 'wall': 1, 'Dumpty': 2, 'men': 1, 'had': 1, 'all': 1, 'together': 1, \"King's\": 2, 'horses': 1, 'All': 1, \"Couldn't\": 1, 'fall': 1, 'and': 1, 'the': 2, 'put': 1, 'sat': 1}"
+
+python example.py > output &
+python mincemeat.py -p changeme localhost
+
+diff <(cat output) <(echo $EXPECTED)
